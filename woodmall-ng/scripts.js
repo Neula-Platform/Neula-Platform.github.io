b81a8d8ca5585fccf18d5f1305576054e691f93e
Marcin Pieciukiewicz <marcin.pieciukiewicz@jtweston.pl>
2022-11-03 11:05:55 +0100

    Font size reduced

--- woodmall-ng/scripts.js
+++ woodmall-ng/scripts.js
@@ -1,7 +1,7 @@
 const engineMainUrl = "screens/#/";
 const functionsUrl = "https://woodmall.neula.cloud/api/functions/wood_auctions/"
 const notSecretToken = "5c7285017643837e7b4eb4c60a23ae404f20d6b1ebefaffa4a722a98d06def176730";
-const portletSettings = "/n-background-color=f6f4f3/n-font-size=15";
+const portletSettings = "/n-background-color=f6f4f3/n-font-size=14";
 
 // function initSessionId() {
 //     let sessionId = sessionStorage.getItem("sessionId");
